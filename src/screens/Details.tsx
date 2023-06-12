eb6469de99fd4b2dd4f68d7eb23839f5c6c78724
nykael <nykaelccardial@gmail.com>
2023-06-12 12:38:04 -0300

    get by params

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,10 +1,23 @@
-import { VStack} from "native-base"
+import { Text, VStack} from "native-base"
+import { useRoute } from "@react-navigation/native"
+
 import { Header } from "../components/Header"
 
+interface RouteParams {
+    orderId: string
+}
+
 export function Details () {
+    const route = useRoute()
+    const {orderId} = route.params as RouteParams
+
     return (
         <VStack flex={1} bg="gray.700"> 
             <Header title="Solicitação" />
+
+            <Text color="gray.100">
+                {orderId}
+            </Text>
         </VStack>
     )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -11,13 +11,24 @@ import { Order, OrderProps } from "../components/Order";
 
 export function Home() {
     const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open')
-    const [orders, setOrders] = useState<OrderProps[]>([])
+    const [orders, setOrders] = useState<OrderProps[]>([
+      {
+        id: '123',
+        patrimony: '123456',
+        when: '18/07/2022 às 14:00',
+        status: 'open'
+      }
+    ])
     
     const navigation = useNavigation()
     const {colors} = useTheme()
     
     function handleNewOrder() {
       navigation.navigate('new')
+    }
+
+    function handleOpenDetails(orderId: string){
+      navigation.navigate('details', {orderId})
     }
 
     return (
@@ -73,7 +84,7 @@ export function Home() {
               <FlatList 
                  data={orders}
                  keyExtractor={(item) => item.id}
-                 renderItem={({item}) => <Order data={item} />}
+                 renderItem={({item}) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
                  showsVerticalScrollIndicator={false}
                  contentContainerStyle={{paddingBottom: 100}}
                  ListEmptyComponent={() => (
