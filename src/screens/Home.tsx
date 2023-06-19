9e56b241a4770ccba8e44351baf8c15c062414c6
nykael <nykaelccardial@gmail.com>
2023-06-19 15:25:21 -0300

    Login and LogoOut

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,6 +3,8 @@ import {useNavigation} from '@react-navigation/native'
 import { Center, FlatList, HStack, Heading, IconButton, Text, VStack, useTheme } from "native-base";
 import {SignOut} from 'phosphor-react-native'
 import {ChatTeardropText} from 'phosphor-react-native'
+import { Alert } from "react-native";
+import auth from '@react-native-firebase/auth'
 
 import Logo from '../assets/LogoS.svg'
 import { Filter } from "../components/Filter";
@@ -31,6 +33,15 @@ export function Home() {
       navigation.navigate('details', {orderId})
     }
 
+    function handleLogout() {
+      auth().
+      signOut()
+      .catch((error) => {
+        console.log(error)
+        return Alert.alert('Sair', 'Não foi possívle sair.')
+      })
+    }
+
     return (
         <VStack flex={1} pb={6} bg="gray.700">
             <HStack
@@ -45,6 +56,7 @@ export function Home() {
              <Logo />
 
              <IconButton 
+              onPress={handleLogout}
               icon={<SignOut  size={26} color={colors.gray[300]}/>}
              />
             </HStack>
